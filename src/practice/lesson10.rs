4a8934059c3a268177c672e90287a3f749d8f568
RomanShapran <107122056+RomanShapran@users.noreply.github.com>
2025-05-25 23:35:54 +0300

    Update lesson10.rs

--- src/practice/lesson10.rs
+++ src/practice/lesson10.rs
@@ -1,5 +1,25 @@
-/// max_nest_level
-#[test]
-fn test1() {
 
+fn is_palindrome(x: u32) -> bool {
+    let s = x.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+fn main() {
+    let numbers = [123, 121, 1221, 1, 0, 12321];
+    for &n in &numbers {
+        println!("Is {} a palindrome? {}", n, is_palindrome(n));
+    }
+}
+
+#[test]
+fn test() {
+    let data = [
+        (123, false),
+        (121, true),
+        (1221, true),
+    ];
+
+    data
+        .iter()
+        .for_each(|(n, exp)| assert_eq!(is_palindrome(*n), *exp));
 }
