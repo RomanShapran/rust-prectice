b6104d522b2ba43dff494cb1de7e5384c79a7d78
RomanShapran <107122056+RomanShapran@users.noreply.github.com>
2025-05-25 23:33:28 +0300

    Update lesson05.rs

--- src/practice/lesson05.rs
+++ src/practice/lesson05.rs
@@ -1,5 +1,15 @@
-/// console - chessboard
-#[test]
-fn test1() {
+fn greatest_common_divisor(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
 
+fn main() {
+    let x = 48;
+    let y = 18;
+
+    println!("GCD({}, {}) = {}", x, y, greatest_common_divisor(x, y));
 }
