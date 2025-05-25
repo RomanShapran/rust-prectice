ae9b2f3e938f5bd9d794ea0abb8fa00ff66fba8b
RomanShapran <107122056+RomanShapran@users.noreply.github.com>
2025-05-25 23:32:25 +0300

    Update lesson03.rs

--- src/practice/lesson03.rs
+++ src/practice/lesson03.rs
@@ -1,5 +1,22 @@
-/// console - envelope
-#[test]
-fn test1() {
+const W: usize = 30;
+const H: usize = 15;
 
+fn main() {
+    let mut result = String::new();
+
+    for y in 0..H {
+        for x in 0..W {
+            let ch = if y == 0 || y == H - 1 || x == 0 || x == W - 1 {
+                '*' // рамка
+            } else if x == y * W / H || x == (H - 1 - y) * W / H {
+                '*' // діагоналі
+            } else {
+                ' '
+            };
+            result.push(ch);
+        }
+        result.push('\n');
+    }
+
+    print!("{}", result);
 }
